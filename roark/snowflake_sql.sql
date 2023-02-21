3b1fe878f56706609afd87de5d27b307fc2fb91e
ShaunODonnell <shaun.odonnell@snowflake.com>
2023-02-20 21:06:45 -0500

    minor change

--- roark/snowflake_sql.sql
+++ roark/snowflake_sql.sql
@@ -1,1 +1,14 @@
+use role accountadmin;
+use database roark_demo;
+use warehouse lab_wh;
+
+use schema public;
+
+describe table nbc_loc;
+select * from google_places_reviews limit 10;
+
+select review_rating , review_text, owner_answer  from google_places_reviews 
+where owner_answer is not null ;
+
+select count(query) from google_places_reviews;
 show users;
